02d8f9e84440b0d2e826eecce860399e977f514a
MariaPPresa <mariaperezpresa@gmail.com>
2023-11-02 20:50:27 +0100

    First generate_dinners

--- menu.py
+++ menu.py
@@ -1,31 +1,43 @@
 import sys
 import random
-import sqlite3
+import json
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QGridLayout, QFrame, QHBoxLayout
-
-class MenuSemanal(QWidget):
+    
+class WeeklyMenu(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
         
-    def leer_datos_de_base_de_datos(self):
-        conn = sqlite3.connect("mi_base_de_datos.db")  # Reemplaza "mi_base_de_datos.db" con la ubicación de tu base de datos
-        cursor = conn.cursor()
+    def read_data(self):
+        with open("comidas.json", "r") as f:
+            data = f.read()
 
-        # Realizar una consulta para recuperar datos
-        cursor.execute("SELECT Nombre, Tipo, Sano, Ofi FROM comidas")
+        dishes = json.loads(data)
 
-        # Recuperar los resultados de la consulta
-        resultados = cursor.fetchall()
+        return dishes 
+        
+    def generate_dinners(self):
+        """
+            Genera las 7 cenas aleatorias de la semana.
+        
+            Returns:
+                Una lista de 7 cenas
+                """
 
-        # Procesar los resultados y actualizar la interfaz gráfica
-        for fila in resultados:
-            nombre, tipo, sano, ofi = fila
-            # Actualiza la interfaz gráfica con los datos recuperados
-            # Por ejemplo, podrías asignar los valores a las casillas de texto correspondientes
+        # Leer los datos del JSON
+        dishes = self.read_data()
 
-        # Cerrar la conexión
-        conn.close()
+        # Obtener una lista de las cenas
+        #dinners = [dish["name"] for dish in dishes if int(dish["time"].count("Cena")) > 0]
+        dinners = [dish for dish in dishes]
+        few_dinners = []
+    
+        # Usar una comprensión de lista para obtener solo los nombres
+        nombres_platos = [plato["name"] for plato in dishes]
+
+        # Elegir 7 cenas aleatorias
+        dinners_list = random.sample(nombres_platos, 7)
+        return dinners_list
         
     def initUI(self):
         self.setWindowTitle("Menú Semanal")
@@ -34,66 +46,69 @@ class MenuSemanal(QWidget):
         layout = QGridLayout()
         self.setLayout(layout)
         
-        self.dias_semana = ["LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES", "SÁBADO", "DOMINGO"]
+        self.week_days = ["LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES", "SÁBADO", "DOMINGO"]
         
-        self.desayuno_entries = []
-        self.comida_entries = []
-        self.cena_entries = []
-
-        for dia in self.dias_semana:
-            dia_frame = QFrame(self)
-            dia_frame.setStyleSheet("background-color: #3498db; border-radius: 5px;")  # Cambia el color de fondo y el borde
-            h_layout = QHBoxLayout(dia_frame)
+        self.breakfast_entries = []
+        self.lunch_entries = []
+        self.dinner_entries = []
+        
+        dinnerList = self.generate_dinners()
+        
+        for day in self.week_days:
+            day_frame = QFrame(self)
+            day_frame.setStyleSheet("background-color: #3498db; border-radius: 5px;")  # Cambia el color de fondo y el borde
+            h_layout = QHBoxLayout(day_frame)
     
-            dia_button = QPushButton(dia, dia_frame)
-            dia_button.clicked.connect(lambda checked, dia=dia: self.regenerar_menu(dia))
-            h_layout.addWidget(dia_button)
+            day_button = QPushButton(day, day_frame)
+            day_button.clicked.connect(lambda checked, day=day: self.regenerate_menu(day))
+            h_layout.addWidget(day_button)
     
-            layout.addWidget(dia_frame, 0, self.dias_semana.index(dia))
+            layout.addWidget(day_frame, 0, self.week_days.index(day))
             
-            desayuno_label = QLabel("Desayuno")
-            layout.addWidget(desayuno_label, 1, self.dias_semana.index(dia))
-            desayuno_entry = QLineEdit()
-            desayuno_entry.setReadOnly(True)
-            desayuno_entry.setText(str(random.randint(1, 1000)))
-            layout.addWidget(desayuno_entry, 2, self.dias_semana.index(dia))
-            self.desayuno_entries.append(desayuno_entry)
+            breakfast_label = QLabel("breakfast")
+            layout.addWidget(breakfast_label, 1, self.week_days.index(day))
+            breakfast_entry = QLineEdit()
+            breakfast_entry.setReadOnly(True)
+            breakfast_entry.setText(str(random.randint(1, 1000)))
+            layout.addWidget(breakfast_entry, 2, self.week_days.index(day))
+            self.breakfast_entries.append(breakfast_entry)
             
-            comida_label = QLabel("Comida")
-            layout.addWidget(comida_label, 3, self.dias_semana.index(dia))
-            comida_entry = QLineEdit()
-            comida_entry.setReadOnly(True)
-            comida_entry.setText(str(random.randint(1, 1000))
+            lunch_label = QLabel("luncha")
+            layout.addWidget(lunch_label, 3, self.week_days.index(day))
+            lunch_entry = QLineEdit()
+            lunch_entry.setReadOnly(True)
+            lunch_entry.setText(str(random.randint(1, 1000))
             )
-            layout.addWidget(comida_entry, 4, self.dias_semana.index(dia))
-            self.comida_entries.append(comida_entry)
+            layout.addWidget(lunch_entry, 4, self.week_days.index(day))
+            self.lunch_entries.append(lunch_entry)
             
-            cena_label = QLabel("Cena")
-            layout.addWidget(cena_label, 5, self.dias_semana.index(dia))
-            cena_entry = QLineEdit()
-            cena_entry.setReadOnly(True)
-            cena_entry.setText(str(random.randint(1, 1000)))
-            layout.addWidget(cena_entry, 6, self.dias_semana.index(dia))
-            self.cena_entries.append(cena_entry)
+            dinner_label = QLabel("dinner")
+            layout.addWidget(dinner_label, 5, self.week_days.index(day))
+            dinner_entry = QLineEdit()
+            dinner_entry.setReadOnly(True)
+            dinner_entry.setText(dinnerList[self.week_days.index(day)])
+            layout.addWidget(dinner_entry, 6, self.week_days.index(day))
+            self.dinner_entries.append(dinner_entry)
 
-    def regenerar_menu(self, dia):
-        dia_index = self.dias_semana.index(dia)
-        desayuno = random.randint(1, 1000)
-        comida = random.randint(1, 1000)
-        cena = random.randint(1, 1000)
-        self.desayuno_entries[dia_index].setText(str(desayuno))
-        self.comida_entries[dia_index].setText(str(comida))
-        self.cena_entries[dia_index].setText(str(cena))
+
+
+    def regenerte_menu(self, day):
+        day_index = self.week_days.index(day)
+        breakfast = random.randint(1, 1000)
+        lunch = random.randint(1, 1000)
+        dinner = random.randint(1, 1000)
+        self.breakfast_entries[day_index].setText(str(breakfast))
+        self.lunch_entries[day_index].setText(str(lunch))
+        self.dinner_entries[day_index].setText(str(dinner))
 
 def main():
     """
     La funcion tal hace tal
      - app: llama a tal y devuelve esto
     """
-    #novatos
     app = QApplication(sys.argv)
-    menu_semanal = MenuSemanal()
-    menu_semanal.show()
+    weekly_menu = WeeklyMenu()
+    weekly_menu.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
